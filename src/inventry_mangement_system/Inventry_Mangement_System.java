40cfcc826ad56e5377460840207924389964b939
Dinuwan Jayasinghe <97357244+DinuwanJayasinghe@users.noreply.github.com>
2024-12-27 02:48:33 +0530

    Source Files

---
+++ src/inventry_mangement_system/Inventry_Mangement_System.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventry_mangement_system;
+
+/**
+ *
+ * @author Dinuwan Theekshana
+ */
+public class Inventry_Mangement_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new JFrame_1().setVisible(true);
+            }
+        });
+    }
+
+}
